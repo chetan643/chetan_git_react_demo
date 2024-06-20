380164852a99bad5f3aa819bd35772dd100679d8
Your Name <you@example.com>
2024-06-20 17:33:48 -0400

    cosmetic changes

--- react-chetan/src/App.jsx
+++ react-chetan/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1>Chetan Kumar</h1>
       <h2>This is demo react project</h2>
       <h3>This project is for learning purpose and to practice git.</h3>
-      <h4>Added CI/CD workflows to the project</h4>
+      <h3>Added CI/CD workflows to the project</h3>
     </div>
   )
 }
